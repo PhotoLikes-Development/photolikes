9852c48650958f8ac86606a13577c9e86b33b6ff
E1izabeth <e1izabeth.k@outlook.com>
2021-03-15 00:23:59 +0300

    Implement scraper

---
+++ src/config/base_config.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from config.enums.environment import Environment
+
+
+class BaseConfig:
+    VK_API_TOKEN: str
+    START_SCREEN_NAMES: List[str]
+    MONGO_DATABASE: str
+    MONGO_URI: str
+
+
+class ProductionConfig(BaseConfig):
+    ENVIRONMENT = Environment.PRODUCTION
+
+
+class DevelopmentConfig(BaseConfig):
+    ENVIRONMENT = Environment.DEVELOPMENT
+
+    MONGO_DATABASE = "photo_vk_likes_estimation_dev"
+    MONGO_URI = "mongodb://localhost:27017"
