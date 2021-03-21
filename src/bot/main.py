4ef5a29be0707dbfbba315745d1aa3108472ad0f
YogurtTheHorse <yegorf1@gmail.com>
2021-03-22 01:53:57 +0300

    Add abstractions for answering to photos

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,14 +1,21 @@
 import logging
+import numpy
+import PIL
+
 from config import Config
 
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from common.image_processing import dataurl_from_image, preprocess_image
+
+from model.model_loader import ModelLoader
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
+model_loader = ModelLoader()
 
 
 def start(update: Update, _: CallbackContext) -> None:
@@ -19,8 +26,25 @@ def help_command(update: Update, _: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def echo(update: Update, ctx: CallbackContext) -> None:
+    photos_number = len(update.message.photo)
+
+    if not photos_number:
+        update.message.reply_text('Please send image to me')
+        return
+
+    if photos_number > 1:
+        update.message.reply_text('Please send only one photo per message')
+        return
+
+    photo = ctx.bot.get_file(update.message.photo[0].file_id)
+    model = model_loader.get_instance()
+    image_arr = preprocess_image(photo)
+    estimation = model.predict(numpy.array([image_arr]))[0]
+    lo, mid, hi = estimation
+
+    update.message.reply_text(f'There is {lo * 100}% probability that photo will be low-rated, '
+                              f'{mid * 100} average-rated and {hi * 100} high-rated.')
 
 
 def main() -> None:
@@ -36,7 +60,7 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("help", help_command))
 
     # on noncommand i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.photo & ~Filters.command, echo))
 
     # Start the Bot
     updater.start_polling()
